ccd449a1d66422a1d03b338cd7a19d5978b9d0c2
deadseal001 <liwenbo4936@gmail.com>
2022-09-17 00:26:33 -0500

    language function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl= document.querySelector("#language-buttons");
 
 var getUserRepos = function(user){
     //formate the github api url
@@ -81,6 +82,31 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 }
+function getFeaturedRepos(language){
+    var apiUrl = "https://api.github.com/search/repositories?q="+language + "is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function (response) {
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items,language)
+                console.log(data);
+            })
+            console.log(response);
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
 
+var buttonClickHandler=function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if(language){
+        getFeaturedRepos(language);
+        //clear old content
+        repoContainerEl.textContent="";
+    }
+    
+}
 
 $(userFormEl).on("submit",formSubmitHandler);
+$(languageButtonEl).on("click", buttonClickHandler);
